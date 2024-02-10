e914e7664fa94a7da1a8de95a79d1e7840631a4e
Jari Polm <jpolm@rollins.edu>
2024-02-10 12:36:44 +0100

    Material UI and LandingPage

---
+++ src/LandingPage/Header.tsx
@@ -0,0 +1,31 @@
+import {
+    AppBar,
+    Toolbar,
+    Typography
+} from '@material-ui/core';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+  
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      appBar: {
+        backgroundColor: theme.palette.primary.main,
+        marginBottom: theme.spacing(4),
+      }
+    })
+  );
+  
+  
+export default function Header() {
+    const classes = useStyles();
+  
+    return (
+    <AppBar position="relative" className={classes.appBar}>
+        <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+            SiftStats
+        </Typography>
+        </Toolbar>
+    </AppBar>
+    );
+}
+  
