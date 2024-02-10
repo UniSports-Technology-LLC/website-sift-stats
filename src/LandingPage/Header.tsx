40d7d7f446c1b49e756c94dd945303a5d73bf299
Jari Polm <jpolm@rollins.edu>
2024-02-10 12:44:02 +0100

    Name

--- src/LandingPage/Header.tsx
+++ src/LandingPage/Header.tsx
@@ -5,14 +5,14 @@ import {
 } from '@material-ui/core';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
   
-  const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
       appBar: {
         backgroundColor: theme.palette.primary.main,
         marginBottom: theme.spacing(4),
       }
     })
-  );
+);
   
   
 export default function Header() {
