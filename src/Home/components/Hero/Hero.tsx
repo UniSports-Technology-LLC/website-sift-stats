2df306c8f6d5d708feeb92d800c620a682982505
Jari Polm <jpolm@rollins.edu>
2024-03-15 10:02:38 +0100

    added video

--- src/Home/components/Hero/Hero.tsx
+++ src/Home/components/Hero/Hero.tsx
@@ -1,10 +1,8 @@
-import { alpha } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 // import hero from "../../../../assets/hero.png";
-import { imagePaths } from '../../../assets/imagePaths';
 import AppStores from './AppStore';
 
 export default function Hero() {
@@ -59,7 +57,7 @@ export default function Hero() {
           </Typography>
           <AppStores />
         </Stack>
-        <Box
+        {/* <Box
           id="image"
           sx={(theme) => ({
             mt: { xs: 8, sm: 10 },
@@ -80,7 +78,24 @@ export default function Hero() {
                 ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
                 : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
           })}
-        />
+        /> */}
+        <Box
+          sx={() => ({
+            mt: { xs: 8, sm: 10},
+            height: { xs: 200, sm: 500 },
+            width: { xs: '100%', sm: 885 },
+            borderRadius: '10px',
+          })}
+        >
+          <iframe 
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/L-pztpJwIR0?si=rzVUBIp4JNQABAov" 
+            title="YouTube video player" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen
+          />
+        </Box>
       </Container>
     </Box>
   );
